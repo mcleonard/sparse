4f269290f0677bdd5727b5a0fc46a0a7b64c27fa
Mat Leonard <leonard.mat@gmail.com>
2013-05-23 17:04:32 -0700

    Added load_daq and load_exper functions.  Refactored load_file to use the new functions.
    I had a problem loading the experiment protocol data.  Different
    experiments have different protocols with different formats.  So, I had
    to find a more general method for loading in the data.  The data is
    some struct with a bunch of variable fields.  It records the fields,
    and if one is 'param', looks through the fields there too.  It gets the
    data types of the fields too, and uses the field names and data types
    to build a record array.

--- io.py
+++ io.py
@@ -20,28 +20,73 @@ def load_file(filepath, full=False):
         daq_file = os.path.join(datadir, 'daq_' + filename)
         exper_file = filepath
     
-    # Load electrical recording data
-    print daq_file
+    data = load_daq(daq_file)
+    exper, stims = load_exper(exper_file)
+    
+    if full:
+        return data, stims, exper
+    
+    return data, stims
+    
+def load_daq(daq_file, verbose=False):
+    """ Loads the daq information from the daq_*.mat file.  Returns a record
+        array with 'signal', 'trigger', 'stimulus', and 'junk' fields.
+    """
     daq = io.loadmat(daq_file)['daq']
     records = [('signal', 'f8'), ('trigger','f8'), 
                ('stimulus','f8'), ('junk','f8')]
     data = np.zeros(len(daq), dtype = records)
     for (col, _), row in izip(records, daq.T):
-        data[col] = tuple(row)    
+        data[col] = tuple(row)
     
-    # Load experiment protocol    
-    exper = io.loadmat(exper_file, chars_as_strings=True,
-                       struct_as_record=False, squeeze_me=True)
-    exper = exper['exper_002']
-    stimuli = exper.stimulusczar.param.stimuli.value
-    records = [('stimulus', 'S20'), ('stimlength', 'i8'), ('amplitude', 'i8'),
-               ('duration', 'i8'), ('frequency', 'f8'), ('ramp', 'i8')]
-    stims = np.zeros(len(stimuli), dtype = records)
-
-    for i, stim in enumerate(stimuli):
-        stims[i] = (stim.type, stim.stimlength, stim.param.amplitude,
-                    stim.param.duration, stim.param.frequency, stim.param.ramp)
-    if full:
-        return data, stims, exper
+    if verbose:
+        print('{} loaded'.format(daq_file))
     
-    return data, stims
+    return data
+        
+def load_exper(exper_file, verbose=False):
+    """ Loads experiment protocol file and pulls out stimuli information.
+        Returns the exper struct and a record array of stimulus information.
+    """
+    mat = io.loadmat(exper_file, chars_as_strings=True,
+                     struct_as_record=False, squeeze_me=True)
+    
+    # Different protocols have different names.  They seem to be titled
+    # 'exper_*', so I'm looking for a key with 'exper' in it.               
+    exper_key = [ key for key in mat.viewkeys() if 'exper' in key ][0]
+    exper = mat[exper_key]
+    
+    stims = exper.stimulusczar.param.stimuli.value
+    
+    # Get the fields and dtypes from stims
+    stim = stims[0]
+    fields = stim._fieldnames[:]
+    dtypes = [ np.dtype(type(stim.__getattribute__(field))) 
+               for field in fields if field != 'param']
+    
+    # Do the same for 'param' field
+    if 'param' in fields:
+        params = stim.param._fieldnames[:]
+        fields.remove('param')
+        fields.extend(params)
+        param_dtypes = [ np.dtype(type(stim.param.__getattribute__(par))) 
+                         for par in params ]
+    dtypes = np.concatenate([dtypes, param_dtypes])
+    
+    # Switch all unicode fields to string fields.
+    dtypes[np.where(dtypes == '<U0')]=np.dtype('S50')
+    
+    # Now make the record array and fill it.
+    records = zip(fields, dtypes)
+    stim_arr = np.zeros(len(stims), dtype=records)
+    for i, stim in enumerate(stims):
+        for name in stim_arr.dtype.names:
+            if name in stim._fieldnames:
+                stim_arr[name][i] = stim.__getattribute__(name)
+            elif name in stim.param._fieldnames:
+                stim_arr[name][i] = stim.param.__getattribute__(name)
+    
+    if verbose:
+        print('{} loaded'.format(exper_file))
+    
+    return exper, stim_arr
